56a800a09c5de285224b8b067563b89cbbb28dda
2021MT13005-Divesh <2021MT13005@wilp.bits-pilani.ac.in>
2022-04-12 13:58:16 +0530

    Added files for Recharge functionality; Modified dto classes

--- src/main/java/com/dda/assignment/repository/CustomerPlansRepository.java
+++ src/main/java/com/dda/assignment/repository/CustomerPlansRepository.java
@@ -1,8 +1,16 @@
 package com.dda.assignment.repository;
 
-import com.dda.assignment.dto.Customer;
 import com.dda.assignment.dto.CustomerPlans;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CustomerPlansRepository  extends JpaRepository<CustomerPlans, java.math.BigInteger> {
+import java.math.BigInteger;
+import java.util.List;
+
+public interface CustomerPlansRepository extends JpaRepository<CustomerPlans, java.math.BigInteger> {
+
+    @Query("SELECT cp FROM CustomerPlan WHERE cp.customerId = :customerId AND cp.planId = :planId")
+    List<CustomerPlans> getCustomerPlansByCustomerIdAndPlanId(@Param("customerId") BigInteger customerId,
+                                                              @Param("planId") BigInteger planId);
 }
