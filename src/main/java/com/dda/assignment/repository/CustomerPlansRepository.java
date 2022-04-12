ba85f9e5f701f2e13c0a47741264896dd89e8cdf
2021MT13005-Divesh <2021MT13005@wilp.bits-pilani.ac.in>
2022-04-12 19:23:31 +0530

    Added controller and services for Recharge

--- src/main/java/com/dda/assignment/repository/CustomerPlansRepository.java
+++ src/main/java/com/dda/assignment/repository/CustomerPlansRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.repository.query.Param;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerPlansRepository extends JpaRepository<CustomerPlans, java.math.BigInteger> {
 
     @Query("SELECT cp FROM CustomerPlan WHERE cp.customerId = :customerId AND cp.planId = :planId")
-    List<CustomerPlans> getCustomerPlansByCustomerIdAndPlanId(@Param("customerId") BigInteger customerId,
-                                                              @Param("planId") BigInteger planId);
+    Optional<CustomerPlans> getCustomerPlansByCustomerIdAndPlanId(@Param("customerId") String customerId,
+                                                                 @Param("planId") String planId);
 }
